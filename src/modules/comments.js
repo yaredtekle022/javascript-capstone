b69e017448e8131d6388cad89a2bd9416e374491
Jawad <Jawad.Nazari@outlook.com>
2023-04-29 08:11:08 +0500

    Updating functions to ES6

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -6,7 +6,7 @@ const getCommentData = async (idMeals) => {
   return response;
 };
 
-async function openPopup(data) {
+const openPopup = async (data) => {
   const container = document.querySelector('.comments');
   const popup = `<div class="outside">
     <p class="closeBtn">X</p>
@@ -85,11 +85,12 @@ document.addEventListener('click', async (e) => {
   }
 });
 
-function closePopup() {
+const closePopup = () => {
   const container = document.querySelector('.comments');
   container.style.display = 'none';
   container.innerHTML = '';
-}
+};
+
 
 document.addEventListener('click', (e) => {
   if (e.target.classList.contains('closeBtn')) {
